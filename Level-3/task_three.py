c1233f37c7f68624931ff70dfdef69856f2efafd
Bill <bvelizmera@gmail.com>
2024-11-01 17:55:10 +0000

    I fixed the output, now it is adding properly, but failed all test

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -5,20 +5,22 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 
 
 def generate_invoice(receipt_string: str) -> str:
+    lines = receipt_string.strip().split('\n')
+    new_receipt_text = '\n'.join(lines[:-1])
 
-    separate_items_invoice = receipt_string.split("\n")
-    print(separate_items_invoice)
+    separate_items_invoice = new_receipt_text.split("\n")
     total_vat = 0
     price_no_vat = 0
     for items_invoice in separate_items_invoice:
         new_item = items_invoice.split("£")
-        price_no_vat += float(new_item[1])
+        price_no_vat += (float(new_item[1]))
         total_vat += float(new_item[1]) * 0.2
     rounded_vat = round(total_vat, 2)
+
+    total_new_price = price_no_vat + rounded_vat
     
-    lines = receipt_string.strip().split('\n')
-    new_receipt_text = '\n'.join(lines[:-1])
-    return f"VAT RECEIPT \n\n" + new_receipt_text + f"\nTotal: £{price_no_vat:.2f}Total inc VAT: £{rounded_vat:.2f}"
+
+    return f"VAT RECEIPT \n\n" + new_receipt_text + f"\n\nTotal: £{price_no_vat:.2f}\nVAT: £{total_vat:.2f}\nTotal inc VAT: £{total_new_price:.2f}"
     
 
 
