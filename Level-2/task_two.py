a515de38347f22cbe80f90b7ed8dabb07dc644cc
Bill <bvelizmera@gmail.com>
2024-11-01 17:05:59 +0000

    Task 2 completed, all tests passed

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -5,27 +5,36 @@ The only variable you are allowed to use in the global scope is the basket below
 
 basket = []
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
-
-
 def add_to_basket(item: dict) -> list:
-    pass
-
-
-
+    """This function will add the items to the basket"""
+    basket.append(item)
+    return basket
 def generate_receipt(basket: list) -> str:
-    pass
+    """It will generate a receipt to be printed later on"""
+    receipt = ""
+    single_items = []
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+    for item in basket:
+        if item not in single_items:
+            single_items.append(item)
 
+    for individual_item in single_items:
+
+        receipt += (f"{individual_item["name"]} x {basket.count(individual_item)} - £{individual_item["price"]*basket.count(individual_item):.2f}\n")
+    total_price = 0
+    if len(basket) == 0:
+        return "Basket is empty"
+    for item in basket:
+        price = item["price"]
+        total_price += price
+    return  receipt + f"Total: £{total_price:.2f}"# return the receipt string
+
+def convert_price_to_str(price:float) -> str:
+    """This function converts the prices to a string or to free if value is 0"""
+    if price == 0:
+        return "Free"
+
+    return f"£{price:.2f}"
 
 if __name__ == "__main__":
     add_to_basket({
@@ -45,3 +54,5 @@ if __name__ == "__main__":
         "price": 1.20
     })
     print(generate_receipt(basket))
+
+# print(items_counter(basket))
